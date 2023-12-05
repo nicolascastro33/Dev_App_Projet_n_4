d023a8de1a3d5134d7ef0c2bfc1962b75dc86d67
Nicolas Castro <castro.nicolasalain@gmail.com>
2023-12-05 10:06:46 +0100

    Modification page modale fermeture et debut fonction vérification formulaire

--- script/modal.js
+++ script/modal.js
@@ -10,7 +10,10 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+// const formData = document.querySelectorAll(".formData");
+const closeButton = document.querySelector('.close');
+const form = document.querySelector("form");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +22,33 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal event 
+closeButton.addEventListener("click", () => {
+    modalbg.style.display = "none";
+})
+
+// Vérif nom et prénom
+function checkName(name){
+  const regexName = new RegExp("^[a-zA-Z-]+$")
+  if(name.length >= 2 && regexName.test(name)){
+    console.log("oui")
+  }else{
+    console.log("non")
+  }
+}
+// envoie form
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const allInputForm = document.querySelectorAll(".formData input");
+  console.log(allInputForm)
+
+  for(let i = 0; i < allInputForm.length; i++) {
+    input = allInputForm[i]
+    if(input.type === "text"){
+      checkName(input.value)
+    }
+  }
+})
 
 
